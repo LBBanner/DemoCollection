f39d46f041cacb574ba8b62922df0c7f70125475
凌斌 <lingbin@ubtrobot.com>
2017-11-28 17:33:04 +0800

    摇杆 demo

--- DemoCollection/DemoCollection/RockingBarController/RockingBarController.swift
+++ DemoCollection/DemoCollection/RockingBarController/RockingBarController.swift
@@ -6,13 +6,38 @@
 //  Copyright © 2017年 lingbinDemo. All rights reserved.
 //
 
+/**
+ 
+ 原生：
+ oc：
+ https://github.com/jayZhangh/ZLRocker
+ https://github.com/liushuangddu/LSRockingBar
+ https://github.com/mxdhqm/QMJoyStick
+ https://github.com/xingjizhao/JoyStickView
+ swift：
+ https://github.com/Hidebush/YHHandShankView
+ 
+ 基于SpriteKit
+ oc
+ https://github.com/lookdczar/sprite_plane_demo1
+ https://github.com/QiaokeZ/HSKJoystick
+ 
+ */
+
 import UIKit
 
 class RockingBarController: UIViewController {
 
+    @IBOutlet weak var ZLRockerView: ZLRocker! //
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        ZLRockerView.delegate = self
     }
-    
+
+}
+extension RockingBarController: ZLRockerDelegate {
+    func rockerDidChangeDirection(_ rocker: ZLRocker!) {
+        print("direction = \(rocker.direction)")
+    }
 }
